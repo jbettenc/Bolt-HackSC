63026bc255d7544f6c3ad78df0d69cc2d8684d4b
Jordan Bettencourt <jbettenc@usc.edu>
2021-02-19 21:09:18 -0800

    Initial commit

---
+++ app/src/main/java/com/cyclone/bolt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cyclone.bolt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
